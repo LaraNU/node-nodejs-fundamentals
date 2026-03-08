5fc98a6fbb10a68eab2d7fbbb75274de0952388c
Larisa Novozhilova <kara4516@gmail.com>
2026-03-09 00:23:04 +0300

    feat: release lineNumberer function

--- src/streams/lineNumberer.js
+++ src/streams/lineNumberer.js
@@ -1,8 +1,29 @@
+import { Transform } from "stream";
+
 const lineNumberer = () => {
-  // Write your code here
-  // Read from process.stdin
-  // Use Transform Stream to prepend line numbers
-  // Write to process.stdout
+  let lineNumber = 1;
+  let remaining = "";
+
+  const transform = new Transform({
+    transform(chunk, encoding, callback) {
+      const data = remaining + chunk.toString();
+      const lines = data.split("\n");
+      remaining = lines.pop();
+      for (const line of lines) {
+        this.push(`${lineNumber} | ${line}\n`);
+        lineNumber++;
+      }
+      callback();
+    },
+    flush(callback) {
+      if (remaining) {
+        this.push(`${lineNumber} | ${remaining}\n`);
+      }
+      callback();
+    },
+  });
+
+  process.stdin.pipe(transform).pipe(process.stdout);
 };
 
 lineNumberer();
